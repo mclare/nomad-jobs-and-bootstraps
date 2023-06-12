fe4cbf740a3ee99adc6b5a4131a4d17ba6711f6f
Matt Clare <matt@mattclare.ca>
2023-06-12 12:32:12 -0400

    Update bootstrap.sh
    Remove PyOpenSSL and use native SSL support
    as per https://github.com/jamesoff/simplemonitor/releases/tag/v1.12.0

--- simplemonitor/bootstrap.sh
+++ simplemonitor/bootstrap.sh
@@ -4,18 +4,16 @@ apt update
 apt upgrade -y
 apt install -y  inetutils-ping python3-pip dnsutils
 date
-/usr/local/bin/python -m pip install --upgrade pip
-pip install simplemonitor;
+/usr/local/bin/python -m pip install --upgrade pip --no-cache-dir
+pip install simplemonitor --no-cache-dir
 date
 cd /config/simplemonitor/
 echo "Doing config test"
 simplemonitor -vf /config/simplemonitor/monitor.ini -t
-date
-echo "Forcing pyOpenSSL upgrade"
-pip install pyOpenSSL --upgrade
 date
 echo "Running Simple Monitor"
 simplemonitor -vf /config/simplemonitor/monitor.ini
 
 # Should see bootstrap output in Nomad job's stdout
 # Should see simplemonitor's output in Nomad job's stderr
+# If you need to re-run in image apt install -y screen
